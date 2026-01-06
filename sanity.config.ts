ce49a4b50af955bf82322a3e9b244d0808c0bf49
siraajul <shirajulislamparvez@gmail.com>
2026-01-06 09:22:56 +0600

    refactor: update schemaTypes import path to include index file

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
-import { schemaTypes } from "./sanity/schemaTypes";
+import { schemaTypes } from "./sanity/schemaTypes/index";
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
