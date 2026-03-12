57086bf4cfa28f27999f9b0b87241a472f20eb29
siraajul <shirajulislamparvez@gmail.com>
2026-03-12 09:44:57 +0600

    fix!: restore ESM support and implement absolute-safe locators for CI/CD

--- __checks__/home.check.ts
+++ __checks__/home.check.ts
@@ -1,5 +1,9 @@
-import { BrowserCheck } from 'checkly/constructs'
 import path from 'path'
+import { fileURLToPath } from 'url'
+import { BrowserCheck } from 'checkly/constructs'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 new BrowserCheck('homepage-check', {
   name: 'Homepage Check',
