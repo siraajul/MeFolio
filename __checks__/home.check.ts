49a6109776885c8ffb8d503ea594f6cd9911cd9e
siraajul <shirajulislamparvez@gmail.com>
2026-03-12 17:37:22 +0600

    refactor: Migrate Checkly configuration to TypeScript, remove module type from package.json, and clean up home check imports.

--- __checks__/home.check.ts
+++ __checks__/home.check.ts
@@ -1,9 +1,5 @@
 import path from 'path'
-import { fileURLToPath } from 'url'
 import { BrowserCheck } from 'checkly/constructs'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 new BrowserCheck('homepage-check', {
   name: 'Homepage Check',
